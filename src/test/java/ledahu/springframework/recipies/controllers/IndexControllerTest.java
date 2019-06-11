dca4d6aea2a533660c7a179462918db90d84e812
christophe.seguinot <christophe.seguinot@gmail.com>
2019-06-11 16:28:20 +0200

    Use of ArgumentCaptor in unit test of index controller

--- src/test/java/ledahu/springframework/recipies/controllers/IndexControllerTest.java
+++ src/test/java/ledahu/springframework/recipies/controllers/IndexControllerTest.java
@@ -1,12 +1,17 @@
 package ledahu.springframework.recipies.controllers;
 
 
+import ledahu.springframework.recipies.domain.Recipe;
 import ledahu.springframework.recipies.services.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -29,9 +34,19 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(5L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
         assertEquals("index", indexController.getIndexPage(model));
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        assertEquals(2, argumentCaptor.getValue().size());
     }
 }
